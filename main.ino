67cfd7b4ce8a53d62742618a3f6d67b6661d16a7
Liuhao233 <56968546+Liuhao233@users.noreply.github.com>
2020-11-13 22:55:29 +0800

    Add files via upload

--- main.ino
+++ main.ino
@@ -11,8 +11,8 @@
 
 ChainableLED leds(19, 18, NUM_OF_LED);
 
-int sw1pin = 6; //这个是顺序模式开关
-int sw2pin = 7; //黄灯闪烁开关
+int sw1pin = 6; //黄灯闪烁开关
+int sw2pin = 7; //这个是顺序模式开关
 int sw3pin = 15;
 int buzzlepin=10;
 
@@ -22,13 +22,44 @@ NOTE_E6,NOTE_D6,NOTE_G6,NOTE_G6,NOTE_A6,NOTE_A6,NOTE_B6,NOTE_A6,NOTE_E6,NOTE_D6,
 NOTE_G6,NOTE_A6,NOTE_A6,NOTE_B6,NOTE_G6,NOTE_D6,NOTE_E6,NOTE_D6,NOTE_G6,NOTE_G6,NOTE_A6,NOTE_G6};
 
 int dura[]={2,2,1,1,2,2,2,2,2,2,2,1,1,2,2,2,2,2,4,1,1,2,2,2,2,2,2,2,1,1,2,2,2,8};
+//
+int tune_2[] = //简谱频率
+{
+  NOTE_F4,NOTE_F4,NOTE_F4,NOTE_F4,NOTE_F6,NOTE_F6,NOTE_F6,NOTE_F6,
+  NOTE_F5,NOTE_F5,NOTE_F5,NOTE_F5,NOTE_FH1,NOTE_FH1,NOTE_FH1,NOTE_FH1,
+  NOTE_FH2,NOTE_FH2,NOTE_FH2,NOTE_FH2,NOTE_FH2,NOTE_FH2,NOTE_FH2,NOTE_FH2,
+  NOTE_F5,NOTE_F4,NOTE_F3,NOTE_F1,NOTE_F2,NOTE_0,NOTE_F2,NOTE_F6,
+  NOTE_F5,NOTE_0,NOTE_F4,NOTE_0,NOTE_F3,NOTE_0,NOTE_F3,NOTE_F3,NOTE_F5,NOTE_0,NOTE_F4,NOTE_F3,NOTE_F2,NOTE_0,NOTE_F2,NOTE_FH4,
+  NOTE_FH3, NOTE_FH4, NOTE_FH3, NOTE_FH4, NOTE_F2, NOTE_0,NOTE_F2,NOTE_FH4, NOTE_FH3, NOTE_FH4, NOTE_FH3, NOTE_FH4, NOTE_F2, NOTE_0,NOTE_F2,NOTE_F6,
+  NOTE_F5,NOTE_0,NOTE_F4,NOTE_0,NOTE_F3,NOTE_0,NOTE_F3,NOTE_F3,NOTE_F5,NOTE_0,NOTE_F4,NOTE_F3,NOTE_F2,NOTE_0,NOTE_F2,NOTE_FH4,
+  NOTE_FH3, NOTE_FH4, NOTE_FH3, NOTE_FH4, NOTE_F2, NOTE_0,NOTE_F2,NOTE_FH4,NOTE_FH3, NOTE_FH4, NOTE_FH3, NOTE_FH4,
+};
+float durt_2[]=                   //根据简谱列出各节拍
+{
+  0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5, 
+  0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5, 
+  0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5, 
+  0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5, 
+  0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5, 
+  0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5, 
+  0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5, 
+  0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+  0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5, 
+  0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5, 
+  0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5, 
+  0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5
+};
 
+int length;//这里定义一个变量，后面用来表示共有多少个音符 
+
+//
 int len=sizeof(note)/sizeof(int);
-
+int infraredpin=2;
 void setup() {
   pinMode(sw1pin, INPUT);
   pinMode(sw2pin, INPUT);
-  //pinMode(sw3pin,INPUT);
+  pinMode(sw3pin,INPUT);
+  pinMode(infraredpin,INPUT);
   leds.init();//初始化时已经设置灭
   leds.setColorRGB(0, 0, 0, 0);
   pinMode(buzzlepin,OUTPUT);
@@ -38,14 +69,18 @@ void setup() {
     delay(200*dura[i]);
   }
   noTone(buzzlepin);
+  length = sizeof(tune_2)/sizeof(tune_2[0]);
 }
 int key1val = 0;
 int key2val = 0;
 int key3val = 0;
+
 int count1 = 0;
 int count2 = 0;
 void loop() {
+
   //控制信号
+
   if (!digitalRead(sw1pin)) {
     key1val = 1;
     if (key2val) {
@@ -60,6 +95,20 @@ void loop() {
       count1 = 0;
     }
   }
+  if(!digitalRead(infraredpin)){
+    for(int x=0;x<length;x++)
+  {
+    tone(buzzlepin,tune_2[x]);
+    if(x%2)
+    leds.setColorRGB(0, 255, 0,0);
+    else
+    leds.setColorRGB(0,0, 0, 0);
+    if(!digitalRead(sw3pin))break;
+    delay(500*durt_2[x]);  
+  }
+    noTone(buzzlepin);
+    leds.setColorRGB(0,0, 0, 0);
+    }
   if (key1val && !((count1 / YELLOW_INTERVAL) % 2))
     leds.setColorRGB(0, 255, 0, 180);
   if (key1val && ((count1 / YELLOW_INTERVAL) % 2))
@@ -80,5 +129,8 @@ void loop() {
     }
     ++count2;
   }
+  
+
 
 }
+
--- pitches.h
+++ pitches.h
@@ -8,7 +8,6 @@
 #define NOTE_D4 294
 #define NOTE_DS4 311
 #define NOTE_E4 330
-#define NOTE_F4 349
 #define NOTE_FS4 370
 #define NOTE_G4 392
 #define NOTE_GS4 415
@@ -20,7 +19,6 @@
 #define NOTE_D5 587
 #define NOTE_DS5 622
 #define NOTE_E5 659
-#define NOTE_F5 698
 #define NOTE_FS5 740
 #define NOTE_G5 784
 #define NOTE_GS5 831
@@ -32,10 +30,39 @@
 #define NOTE_D6 1175
 #define NOTE_DS6 1245
 #define NOTE_E6 1319
-#define NOTE_F6 1397
 #define NOTE_FS6 1480
 #define NOTE_G6 1568
 #define NOTE_GS6 1661
 #define NOTE_A6 1760
 #define NOTE_AS6 1865
 #define NOTE_B6 1976
+
+//F调
+#define NOTE_F1 350
+#define NOTE_F2 393
+#define NOTE_F3 441
+#define NOTE_F4 495
+#define NOTE_F5 556
+#define NOTE_F6 624
+#define NOTE_F7 661
+#define NOTE_0  -1
+#define NOTE_FL1 175
+#define NOTE_FL2 196
+#define NOTE_FL3 221
+#define NOTE_FL4 234
+#define NOTE_FL5 262
+#define NOTE_FL6 294
+#define NOTE_FL7 330
+
+#define NOTE_FH1 700
+#define NOTE_FH2 786
+#define NOTE_FH3 882
+#define NOTE_FH4 935
+#define NOTE_FH5 1049
+#define NOTE_FH6 1178
+#define NOTE_FH7 1322
+#define WHOLE 1
+#define HALF 0.5
+#define QUARTER 0.25
+#define EIGHTH 0.25
+#define SIXTEENTH 0.625
